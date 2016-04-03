7b1364bc3c564f8b6b8e6aa3b4e0b23e6f2e6697
Jun Hanamaki <jun.hanamaki@gmail.com>
2016-04-03 23:38:05 +0100

    stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,74 +2,70 @@ const webpack = require('webpack');
 const resolve = require('path').resolve;
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const conf = {
-
-  production: {
-    entry: resolve(__dirname, 'src/index.js'),
-    output: {
-      path: resolve(__dirname, 'dist'),
-      filename: 'don-chart.min.js'
-    },
-    module: {
-      loaders: [
-        { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
-        { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
-        { loader: ExtractTextPlugin.extract('style', 'css'), test: /\.css$/, exclude: /node_modules/ }
-      ]
-    },
-    externals: {
-      jquery: 'jQuery'
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin(),
-      new ExtractTextPlugin('don-chart.min.css')
+const production = {
+  entry: resolve(__dirname, 'src/index.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'don-chart.min.js'
+  },
+  module: {
+    loaders: [
+      { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
+      { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
+      { loader: ExtractTextPlugin.extract('style', 'css'), test: /\.css$/, exclude: /node_modules/ }
     ]
   },
-
-  development: {
-    entry: resolve(__dirname, 'src/index.js'),
-    output: {
-      path: resolve(__dirname, 'dist'),
-      filename: 'don-chart.js'
-    },
-    module: {
-      loaders: [
-        { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
-        { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
-        { loader: ExtractTextPlugin.extract('style', 'css'), test: /\.css$/, exclude: /node_modules/ }
-      ]
-    },
-    externals: {
-      jquery: 'jQuery'
-    },
-    plugins: [
-      new ExtractTextPlugin('don-chart.css')
-    ],
-    devtool: 'hidden-source-map'
+  externals: {
+    jquery: 'jQuery'
   },
-
-  dev_server: {
-    entry: resolve(__dirname, 'src/index.js'),
-    output: {
-      path: resolve(__dirname, 'dist'),
-      filename: 'don-chart.js'
-    },
-    module: {
-      preLoaders:[
-        { loader: 'eslint', test: /\.js$/, exclude: /node_modules/ }
-      ],
-      loaders: [
-        { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
-        { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
-        { loader: 'style!css', test: /\.css$/, exclude: /node_modules/ }
-      ]
-    },
-    externals: {
-      jquery: 'jQuery'
-    },
-    devtool: 'inline-source-map'
-  }
-
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('don-chart.min.css')
+  ]
 };
 
-module.exports = conf[process.env.NODE_ENV || 'development'];
+const development = {
+  entry: resolve(__dirname, 'src/index.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'don-chart.js'
+  },
+  module: {
+    loaders: [
+      { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
+      { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
+      { loader: ExtractTextPlugin.extract('style', 'css'), test: /\.css$/, exclude: /node_modules/ }
+    ]
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new ExtractTextPlugin('don-chart.css')
+  ],
+  devtool: 'hidden-source-map'
+};
+
+const dev_server = {
+  entry: resolve(__dirname, 'src/index.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'don-chart.js'
+  },
+  module: {
+    preLoaders:[
+      { loader: 'eslint', test: /\.js$/, exclude: /node_modules/ }
+    ],
+    loaders: [
+      { loader: 'babel', test: /\.js$/, exclude: /node_modules/ },
+      { loader: 'raw', test: /\.html$/, exclude: /node_modules/ },
+      { loader: 'style!css', test: /\.css$/, exclude: /node_modules/ }
+    ]
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  devtool: 'inline-source-map'
+};
+
+module.exports = eval(process.env.NODE_ENV || 'development');
