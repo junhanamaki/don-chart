f3c26809e33d3afca059c5f451fc93e2e5310882
Jun Hanamaki <jun.hanamaki@gmail.com>
2016-04-03 14:52:07 +0100

    refac

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const webpack = require('webpack');
 const resolve = require('path').resolve;
 
 new webpack.ProvidePlugin({
-  jQuery: "jquery",
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery'
 });
 
-const configurations = {
+const conf = {
 
   production: {
     entry: resolve(__dirname, 'src/index.js'),
@@ -44,10 +46,10 @@ const configurations = {
   },
 
   dev_server: {
-    entry: resolve(__dirname, 'src/index.js'),
+    entry: resolve(__dirname, 'example/index.js'),
     output: {
       path: resolve(__dirname, 'example/js'),
-      filename: 'don-chart.js'
+      filename: 'bundle.js'
     },
     module: {
       preLoaders:[
@@ -57,9 +59,12 @@ const configurations = {
         { loader: 'babel', test: /\.js$/, exclude: /node_modules/ }
       ]
     },
+    externals: {
+      jquery: "jQuery"
+    },
     devtool: 'inline-source-map'
   }
 
 };
 
-module.exports = configurations[process.env.NODE_ENV || 'development'];
+module.exports = conf[process.env.NODE_ENV || 'development'];
